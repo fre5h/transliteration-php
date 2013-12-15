b6280fcdde9f78aab9e0382d3337e85fc442359d
Artem Genvald <genvaldartem@gmail.com>
2013-12-15 18:53:27 +0200

    Init library

---
+++ src/Fresh/Transliteration/UkrainianToEnglish.php
@@ -0,0 +1,134 @@
+<?php
+/*
+ * This file is part of the Transliteration library
+ *
+ * (c) Artem Genvald <genvaldartem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fresh\Transliteration;
+
+/**
+ * Transliteration from Ukrainian to English
+ *
+ * @author Artem Genvald <genvaldartem@gmail.com>
+ * @see    http://zakon1.rada.gov.ua/laws/show/55-2010-%D0%BF Resolution #55 of the Cabinet of Ministers of Ukraine
+ */
+class UkrainianToEnglish
+{
+    /**
+     * @var array Rules of transliteration from Ukrainian to English
+     */
+    protected $ukrainianToEnglish = [
+        ['А', 'A'],
+        ['Б', 'B'],
+        ['В', 'V'],
+        ['Г', 'H'],
+        ['Ґ', 'G'],
+        ['Д', 'D'],
+        ['Е', 'E'],
+        ['Є', 'Ye'],
+        ['Ж', 'Zh'],
+        ['З', 'Z'],
+        ['И', 'Y'],
+        ['І', 'I'],
+        ['Ї', 'Yi'],
+        ['Й', 'Y'],
+        ['К', 'K'],
+        ['Л', 'L'],
+        ['М', 'M'],
+        ['Н', 'N'],
+        ['О', 'O'],
+        ['П', 'P'],
+        ['Р', 'R'],
+        ['С', 'S'],
+        ['Т', 'T'],
+        ['У', 'U'],
+        ['Ф', 'F'],
+        ['Х', 'Kh'],
+        ['Ц', 'Ts'],
+        ['Ч', 'Ch'],
+        ['Ш', 'Sh'],
+        ['Щ', 'Shch'],
+        ['Ю', 'Yu'],
+        ['Я', 'Ya'],
+        ['а', 'a'],
+        ['б', 'b'],
+        ['в', 'v'],
+        ['г', 'h'],
+        ['ґ', 'g'],
+        ['д', 'd'],
+        ['е', 'e'],
+        ['є', 'ie'],
+        ['ж', 'zh'],
+        ['з', 'z'],
+        ['и', 'y'],
+        ['і', 'i'],
+        ['ї', 'i'],
+        ['й', 'i'],
+        ['к', 'k'],
+        ['л', 'l'],
+        ['м', 'm'],
+        ['н', 'n'],
+        ['о', 'o'],
+        ['п', 'p'],
+        ['р', 'r'],
+        ['с', 's'],
+        ['т', 't'],
+        ['у', 'u'],
+        ['ф', 'f'],
+        ['х', 'kh'],
+        ['ц', 'ts'],
+        ['ч', 'ch'],
+        ['ш', 'sh'],
+        ['щ', 'shch'],
+        ['ь', ''],
+        ['ю', 'iu'],
+        ['я', 'ia'],
+        ['\'', '']
+    ];
+
+    /**
+     * Transliterate Ukrainian text to English
+     *
+     * According to the rules of transliteration, that are described in the resolution
+     * of the Cabinet of Ministers of Ukraine №55 dated January 27, 2010
+     *
+     * @param string $ukrainianText
+     *
+     * @return string
+     */
+    public function fromUkrainianToEnglish($ukrainianText)
+    {
+        $transliteratedText = '';
+
+        if (mb_strlen($ukrainianText) > 0) {
+            // If found "Zgh|zgh" exception then replace it
+            if ($this->checkForZghException($ukrainianText)) {
+                $ukrainianText = str_replace(['Зг', 'зг'], ['Zgh', 'zgh'], $ukrainianText);
+            }
+            // Transliteration is doing by rendering each letter
+            $transliteratedText = str_replace(
+                array_keys($this->ukrainianToEnglish),
+                array_values($this->ukrainianToEnglish),
+                $ukrainianText
+            );
+        }
+
+        return $transliteratedText;
+    }
+
+    /**
+     * Check text for "Zgh|zgh" exception
+     *
+     * @param string $ukrainianText
+     *
+     * @return bool
+     */
+    protected function checkForZghException($ukrainianText)
+    {
+        return (bool) mb_substr_count($ukrainianText, 'Зг') || (bool) mb_substr_count($ukrainianText, 'зг');
+    }
+}
---
+++ tests/Fresh/Transliteration/UkrainianToEnglishTest.php
@@ -0,0 +1,236 @@
+<?php
+/*
+ * This file is part of the Transliteration library
+ *
+ * (c) Artem Genvald <genvaldartem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Fresh\Transliteration;
+
+use Fresh\Transliteration\UkrainianToEnglish;
+
+/**
+ * Transliteration Test
+ *
+ * @author Artem Genvald <genvaldartem@gmail.com>
+ * @see    http://zakon1.rada.gov.ua/laws/show/55-2010-%D0%BF
+ */
+class TransliterationTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var UkrainianToEnglish
+     */
+    protected $transliteration;
+
+    /**
+     * Set up environment
+     */
+    public function setUp()
+    {
+        $this->transliteration = new UkrainianToEnglish();
+    }
+
+    /**
+     * Test transliteration from ukrainian to english
+     *
+     * @param array $ukrainianText      Ukrainian text
+     * @param array $transliteratedText Expected transliterated text
+     *
+     * @test
+     * @dataProvider transliterationFromUkrainianToEnglishDataProvider
+     */
+    public function transliterationFromUkrainianToEnglish($ukrainianText, $transliteratedText)
+    {
+        $this->assertEquals($transliteratedText, $this->transliteration->fromUkrainianToEnglish($ukrainianText));
+    }
+
+    /**
+     * Data provider for transliteration from ukrainian to english test
+     *
+     * @static
+     * @return array
+     */
+    public function transliterationFromUkrainianToEnglishDataProvider()
+    {
+        return [
+            // Український алфавіт
+            ['А', 'A'],
+            ['Б', 'B'],
+            ['В', 'V'],
+            ['Г', 'H'],
+            ['Ґ', 'G'],
+            ['Д', 'D'],
+            ['Е', 'E'],
+            ['Є', 'Ye'],
+            ['Ж', 'Zh'],
+            ['З', 'Z'],
+            ['И', 'Y'],
+            ['І', 'I'],
+            ['Ї', 'Yi'],
+            ['Й', 'Y'],
+            ['К', 'K'],
+            ['Л', 'L'],
+            ['М', 'M'],
+            ['Н', 'N'],
+            ['О', 'O'],
+            ['П', 'P'],
+            ['Р', 'R'],
+            ['С', 'S'],
+            ['Т', 'T'],
+            ['У', 'U'],
+            ['Ф', 'F'],
+            ['Х', 'Kh'],
+            ['Ц', 'Ts'],
+            ['Ч', 'Ch'],
+            ['Ш', 'Sh'],
+            ['Щ', 'Shch'],
+            ['Ю', 'Yu'],
+            ['Я', 'Ya'],
+            ['а', 'a'],
+            ['б', 'b'],
+            ['в', 'v'],
+            ['г', 'h'],
+            ['ґ', 'g'],
+            ['д', 'd'],
+            ['е', 'e'],
+            ['є', 'ie'],
+            ['ж', 'zh'],
+            ['з', 'z'],
+            ['и', 'y'],
+            ['і', 'i'],
+            ['ї', 'i'],
+            ['й', 'i'],
+            ['к', 'k'],
+            ['л', 'l'],
+            ['м', 'm'],
+            ['н', 'n'],
+            ['о', 'o'],
+            ['п', 'p'],
+            ['р', 'r'],
+            ['с', 's'],
+            ['т', 't'],
+            ['у', 'u'],
+            ['ф', 'f'],
+            ['х', 'kh'],
+            ['ц', 'ts'],
+            ['ч', 'ch'],
+            ['ш', 'sh'],
+            ['щ', 'shch'],
+            ['ь', ''],
+            ['ю', 'iu'],
+            ['я', 'ia'],
+            ['\'', ''],
+
+            // Приклади транслітерації взяті з постанови №55 Кабінету Міністрів України від 27 січня 2010 року
+            // Аа
+            ['Алушта', 'Alushta'],
+            ['Андрій', 'Andrii'],
+            // Бб
+            ['Борщагівка', 'Borshchahivka'],
+            ['Борисенко',  'Borysenko'],
+            // Вв
+            ['Вінниця',   'Vinnytsia'],
+            ['Володимир', 'Volodymyr'],
+            // Гг
+            ['Гадяч',     'Hadiach'],
+            ['Богдан',    'Bohdan'],
+            ['Згурський', 'Zghurskyi'],
+            // Ґґ
+            ['Ґалаґан', 'Galagan'],
+            ['Ґорґани', 'Gorgany'],
+            // Дд
+            ['Донецьк', 'Donetsk'],
+            ['Дмитро',  'Dmytro'],
+            // Ее
+            ['Рівне',  'Rivne'],
+            ['Олег',   'Oleh'],
+            ['Есмань', 'Esman'],
+            // Єє
+            ['Єнакієве', 'Yenakiieve'],
+            ['Гаєвич',   'Haievych'],
+            ['Короп\'є', 'Koropie'],
+            // Жж
+            ['Житомир', 'Zhytomyr'],
+            ['Жанна',   'Zhanna'],
+            ['Жежелів', 'Zhezheliv'],
+            // Зз
+            ['Закарпаття', 'Zakarpattia'],
+            ['Казимирчук', 'Kazymyrchuk'],
+            // Ии
+            ['Медвин',     'Medvyn'],
+            ['Михайленко', 'Mykhailenko'],
+            // Іі
+            ['Іванків',  'Ivankiv'],
+            ['Іващенко', 'Ivashchenko'],
+            // Її
+            ['Їжакевич', 'Yizhakevych'],
+            ['Кадиївка', 'Kadyivka'],
+            ['Мар\'їне', 'Marine'],
+            // Йй
+            ['Йосипівка', 'Yosypivka'],
+            ['Стрий',     'Stryi'],
+            ['Олексій',   'Oleksii'],
+            // Кк
+            ['Київ',      'Kyiv'],
+            ['Коваленко', 'Kovalenko'],
+            // Лл
+            ['Лебедин', 'Lebedyn'],
+            ['Леонід',  'Leonid'],
+            // Мм
+            ['Миколаїв', 'Mykolaiv'],
+            ['Маринич',  'Marynych'],
+            // Нн
+            ['Ніжин',   'Nizhyn'],
+            ['Наталія', 'Nataliia'],
+            // Оо
+            ['Одеса',    'Odesa'],
+            ['Онищенко', 'Onyshchenko'],
+            // Пп
+            ['Полтава', 'Poltava'],
+            ['Петро',   'Petro'],
+            // Рр
+            ['Решетилівка', 'Reshetylivka'],
+            ['Рибчинський', 'Rybchynskyi'],
+            // Сс
+            ['Суми',    'Sumy'],
+            ['Соломія', 'Solomiia'],
+            // Тт
+            ['Тернопіль', 'Ternopil'],
+            ['Троць',     'Trots'],
+            // Уу
+            ['Ужгород', 'Uzhhorod'],
+            ['Уляна',   'Uliana'],
+            // Фф
+            ['Фастів',   'Fastiv'],
+            ['Філіпчук', 'Filipchuk'],
+            // Хх
+            ['Харків',   'Kharkiv'],
+            ['Христина', 'Khrystyna'],
+            // Цц
+            ['Біла Церква', 'Bila Tserkva'],
+            ['Стеценко',    'Stetsenko'],
+            // Чч
+            ['Чернівці', 'Chernivtsi'],
+            ['Шевченко', 'Shevchenko'],
+            // Шш
+            ['Шостка',   'Shostka'],
+            ['Кишеньки', 'Kyshenky'],
+            // Щщ
+            ['Щербухи',   'Shcherbukhy'],
+            ['Гоща',      'Hoshcha'],
+            ['Гаращенко', 'Harashchenko'],
+            // Юю
+            ['Юрій',      'Yurii'],
+            ['Корюківка', 'Koriukivka'],
+            // Яя
+            ['Яготин',     'Yahotyn'],
+            ['Ярошенко',   'Yaroshenko'],
+            ['Костянтин',  'Kostiantyn'],
+            ['Знам\'янка', 'Znamianka'],
+            ['Феодосія',   'Feodosiia']
+        ];
+    }
+}
