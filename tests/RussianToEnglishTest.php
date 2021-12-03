fb3359735575aec8cb8ee52620dd008568ad0460
Artem Henvald <genvaldartem@gmail.com>
2021-12-03 10:25:25 +0200

    Update RussianToEnglishTest.php

--- tests/RussianToEnglishTest.php
+++ tests/RussianToEnglishTest.php
@@ -39,74 +39,72 @@ class RussianToEnglishTest extends TestCase
         self::assertEquals($transliteratedText, $this->transliterator->ruToEn($russianText));
     }
 
-    public function alphabetProvider(): array
+    public function alphabetProvider(): iterable
     {
-        return [
-            ['а', 'a'],
-            ['б', 'b'],
-            ['в', 'v'],
-            ['г', 'g'],
-            ['д', 'd'],
-            ['е', 'e'],
-            ['ё', 'e'],
-            ['ж', 'zh'],
-            ['з', 'z'],
-            ['и', 'i'],
-            ['й', 'y'],
-            ['к', 'k'],
-            ['л', 'l'],
-            ['м', 'm'],
-            ['н', 'n'],
-            ['о', 'o'],
-            ['п', 'p'],
-            ['р', 'r'],
-            ['с', 's'],
-            ['т', 't'],
-            ['у', 'u'],
-            ['ф', 'f'],
-            ['х', 'h'],
-            ['ц', 'ts'],
-            ['ч', 'ch'],
-            ['ш', 'sh'],
-            ['щ', 'sht'],
-            ['ь', ''],
-            ['ы', 'y'],
-            ['ъ', ''],
-            ['э', 'e'],
-            ['ю', 'yu'],
-            ['я', 'ya'],
-            ['А', 'A'],
-            ['Б', 'B'],
-            ['В', 'V'],
-            ['Г', 'G'],
-            ['Д', 'D'],
-            ['Е', 'E'],
-            ['Ж', 'Zh'],
-            ['З', 'Z'],
-            ['И', 'I'],
-            ['Й', 'Y'],
-            ['К', 'K'],
-            ['Л', 'L'],
-            ['М', 'M'],
-            ['Н', 'N'],
-            ['О', 'O'],
-            ['П', 'P'],
-            ['Р', 'R'],
-            ['С', 'S'],
-            ['Т', 'T'],
-            ['У', 'U'],
-            ['Ф', 'F'],
-            ['Х', 'H'],
-            ['Ц', 'Ts'],
-            ['Ч', 'Ch'],
-            ['Ш', 'Sh'],
-            ['Щ', 'Sht'],
-            ['Ь', ''],
-            ['Ы', 'Y'],
-            ['Ъ', ''],
-            ['Э', 'E'],
-            ['Ю', 'Yu'],
-            ['Я', 'Ya'],
-        ];
+        yield ['А', 'A'];
+        yield ['Б', 'B'];
+        yield ['В', 'V'];
+        yield ['Г', 'G'];
+        yield ['Д', 'D'];
+        yield ['Е', 'E'];
+        yield ['Ж', 'Zh'];
+        yield ['З', 'Z'];
+        yield ['И', 'I'];
+        yield ['Й', 'Y'];
+        yield ['К', 'K'];
+        yield ['Л', 'L'];
+        yield ['М', 'M'];
+        yield ['Н', 'N'];
+        yield ['О', 'O'];
+        yield ['П', 'P'];
+        yield ['Р', 'R'];
+        yield ['С', 'S'];
+        yield ['Т', 'T'];
+        yield ['У', 'U'];
+        yield ['Ф', 'F'];
+        yield ['Х', 'H'];
+        yield ['Ц', 'Ts'];
+        yield ['Ч', 'Ch'];
+        yield ['Ш', 'Sh'];
+        yield ['Щ', 'Sht'];
+        yield ['Ь', ''];
+        yield ['Ы', 'Y'];
+        yield ['Ъ', ''];
+        yield ['Э', 'E'];
+        yield ['Ю', 'Yu'];
+        yield ['Я', 'Ya'];
+        yield ['а', 'a'];
+        yield ['б', 'b'];
+        yield ['в', 'v'];
+        yield ['г', 'g'];
+        yield ['д', 'd'];
+        yield ['е', 'e'];
+        yield ['ё', 'e'];
+        yield ['ж', 'zh'];
+        yield ['з', 'z'];
+        yield ['и', 'i'];
+        yield ['й', 'y'];
+        yield ['к', 'k'];
+        yield ['л', 'l'];
+        yield ['м', 'm'];
+        yield ['н', 'n'];
+        yield ['о', 'o'];
+        yield ['п', 'p'];
+        yield ['р', 'r'];
+        yield ['с', 's'];
+        yield ['т', 't'];
+        yield ['у', 'u'];
+        yield ['ф', 'f'];
+        yield ['х', 'h'];
+        yield ['ц', 'ts'];
+        yield ['ч', 'ch'];
+        yield ['ш', 'sh'];
+        yield ['щ', 'sht'];
+        yield ['ь', ''];
+        yield ['ы', 'y'];
+        yield ['ъ', ''];
+        yield ['э', 'e'];
+        yield ['ю', 'yu'];
+        yield ['я', 'ya'];        
     }
 }
