4dc0daabe9e57c561271e89edc7534982b97404d
Artem Henvald <genvaldartem@gmail.com>
2021-12-03 10:18:43 +0200

    Update UkrainianToEnglishTest.php

--- tests/UkrainianToEnglishTest.php
+++ tests/UkrainianToEnglishTest.php
@@ -22,8 +22,7 @@ use PHPUnit\Framework\TestCase;
  */
 class UkrainianToEnglishTest extends TestCase
 {
-    /** @var Transliterator */
-    protected $transliterator;
+    protected Transliterator $transliterator;
 
     protected function setUp(): void
     {
@@ -40,76 +39,74 @@ class UkrainianToEnglishTest extends TestCase
         $this->assertEquals($transliteratedText, $this->transliterator->ukToEn($ukrainianText));
     }
 
-    public function alphabetProvider(): array
+    public function alphabetProvider(): iterable
     {
-        return [
-            ['А', 'A'],
-            ['Б', 'B'],
-            ['В', 'V'],
-            ['Г', 'H'],
-            ['Ґ', 'G'],
-            ['Д', 'D'],
-            ['Е', 'E'],
-            ['Є', 'Ye'],
-            ['Ж', 'Zh'],
-            ['З', 'Z'],
-            ['И', 'Y'],
-            ['І', 'I'],
-            ['Ї', 'Yi'],
-            ['Й', 'Y'],
-            ['К', 'K'],
-            ['Л', 'L'],
-            ['М', 'M'],
-            ['Н', 'N'],
-            ['О', 'O'],
-            ['П', 'P'],
-            ['Р', 'R'],
-            ['С', 'S'],
-            ['Т', 'T'],
-            ['У', 'U'],
-            ['Ф', 'F'],
-            ['Х', 'Kh'],
-            ['Ц', 'Ts'],
-            ['Ч', 'Ch'],
-            ['Ш', 'Sh'],
-            ['Щ', 'Shch'],
-            ['Ю', 'Yu'],
-            ['Я', 'Ya'],
-            ['а', 'a'],
-            ['б', 'b'],
-            ['в', 'v'],
-            ['г', 'h'],
-            ['ґ', 'g'],
-            ['д', 'd'],
-            ['е', 'e'],
-            ['є', 'ie'],
-            ['ж', 'zh'],
-            ['з', 'z'],
-            ['и', 'y'],
-            ['і', 'i'],
-            ['ї', 'i'],
-            ['й', 'i'],
-            ['к', 'k'],
-            ['л', 'l'],
-            ['м', 'm'],
-            ['н', 'n'],
-            ['о', 'o'],
-            ['п', 'p'],
-            ['р', 'r'],
-            ['с', 's'],
-            ['т', 't'],
-            ['у', 'u'],
-            ['ф', 'f'],
-            ['х', 'kh'],
-            ['ц', 'ts'],
-            ['ч', 'ch'],
-            ['ш', 'sh'],
-            ['щ', 'shch'],
-            ['ь', ''],
-            ['ю', 'iu'],
-            ['я', 'ia'],
-            ['\'', ''],
-        ];
+        yield ['А', 'A'];
+        yield ['Б', 'B'];
+        yield ['В', 'V'];
+        yield ['Г', 'H'];
+        yield ['Ґ', 'G'];
+        yield ['Д', 'D'];
+        yield ['Е', 'E'];
+        yield ['Є', 'Ye'];
+        yield ['Ж', 'Zh'];
+        yield ['З', 'Z'];
+        yield ['И', 'Y'];
+        yield ['І', 'I'];
+        yield ['Ї', 'Yi'];
+        yield ['Й', 'Y'];
+        yield ['К', 'K'];
+        yield ['Л', 'L'];
+        yield ['М', 'M'];
+        yield ['Н', 'N'];
+        yield ['О', 'O'];
+        yield ['П', 'P'];
+        yield ['Р', 'R'];
+        yield ['С', 'S'];
+        yield ['Т', 'T'];
+        yield ['У', 'U'];
+        yield ['Ф', 'F'];
+        yield ['Х', 'Kh'];
+        yield ['Ц', 'Ts'];
+        yield ['Ч', 'Ch'];
+        yield ['Ш', 'Sh'];
+        yield ['Щ', 'Shch'];
+        yield ['Ю', 'Yu'];
+        yield ['Я', 'Ya'];
+        yield ['а', 'a'];
+        yield ['б', 'b'];
+        yield ['в', 'v'];
+        yield ['г', 'h'];
+        yield ['ґ', 'g'];
+        yield ['д', 'd'];
+        yield ['е', 'e'];
+        yield ['є', 'ie'];
+        yield ['ж', 'zh'];
+        yield ['з', 'z'];
+        yield ['и', 'y'];
+        yield ['і', 'i'];
+        yield ['ї', 'i'];
+        yield ['й', 'i'];
+        yield ['к', 'k'];
+        yield ['л', 'l'];
+        yield ['м', 'm'];
+        yield ['н', 'n'];
+        yield ['о', 'o'];
+        yield ['п', 'p'];
+        yield ['р', 'r'];
+        yield ['с', 's'];
+        yield ['т', 't'];
+        yield ['у', 'u'];
+        yield ['ф', 'f'];
+        yield ['х', 'kh'];
+        yield ['ц', 'ts'];
+        yield ['ч', 'ch'];
+        yield ['ш', 'sh'];
+        yield ['щ', 'shch'];
+        yield ['ь', ''];
+        yield ['ю', 'iu'];
+        yield ['я', 'ia'];
+        yield ['\'', ''];
     }
 
     public function officialExamplesProvider(): array
@@ -228,11 +225,9 @@ class UkrainianToEnglishTest extends TestCase
         ];
     }
 
-    public function sentencesProvider(): array
+    public function sentencesProvider(): iterable
     {
-        return [
-            ['Добрий день', 'Dobryi den'],
-            ['Привіт світ!', 'Pryvit svit!'],
-        ];
+        yield ['Добрий день', 'Dobryi den'];
+        yield ['Привіт світ!', 'Pryvit svit!'];
     }
 }
